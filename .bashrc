48b824be021526310708bbaea7c7e8368903aa4e
audric-g <audrician@gmail.com>
2026-04-19 07:16:14 -0500

    Initial dot file commit

---
+++ .bashrc
@@ -0,0 +1,16 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# alias ls='ls --color=auto'
+# alias grep='grep --color=auto'
+# PS1='[\u@\h \W]\$ '
+
+# set nvim as my default editor
+export EDITOR=nvim
+
+# set alias config for my dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,23 @@
+local global = vim.g
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.clipboard = "unnamedplus"
+opt.syntax = "on"
+opt.autoindent = true
+opt.cursorline = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.encoding = "UTF-8"
+opt.ruler = true
+opt.title = true
+opt.hidden = true
+opt.wildmenu = true
+opt.showcmd = true
+opt.showmatch = true
+opt.inccommand = "split"
+opt.splitright = true
+opt.termguicolors = true
+opt.scrolloff = 10
