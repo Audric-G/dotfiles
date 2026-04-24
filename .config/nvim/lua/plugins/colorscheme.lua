14e486f72baedf1d3f46698c034a6281c05a48e5
audric-g <audrician@gmail.com>
2026-04-23 23:47:50 -0500

    updated nvim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,22 @@
+-- TOKYDARK
+-- return {
+--   "tiagovla/tokyodark.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--       vim.cmd("colorscheme tokyodark")
+--   end,
+--}
+
+--GRUVBOX
 return {
-    "tiagovla/tokyodark.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme tokyodark")
-    end,
+  "morhetz/gruvbox",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd [[
+      colorscheme gruvbox
+      set background=light
+    ]]
+  end,
 }
